821509f4afd4b69b13e8ba561d6f1ffccfcd52aa
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-08-31 23:31:27 -0300

    preciso reorganizar topicos header

--- index.php
+++ index.php
@@ -24,16 +24,16 @@
         <div class="homes">
             <div class="topico">
                 <a href="vejamais.php">
-                <i id="top-icon" class="fa-solid fa-plus">
-                   Veja mais 
-                </i>                
+                    <i id="top-icon" class="fa-solid fa-plus">
+                        Veja mais
+                    </i>
                 </a>
             </div>
 
             <div class="topico">
                 <a href="index.php">
                     <i id="top-icon" class="fa-solid fa-house">
-                    Home
+                        Home
                     </i>
                 </a>
             </div>
@@ -41,7 +41,7 @@
             <div class="topico">
                 <a href="projetos.php">
                     <i id="top-icon" class="fa-solid fa-bars-progress">
-                    Projetos
+                        Projetos
                     </i>
                 </a>
 
@@ -55,23 +55,27 @@
             <div class="card-sobre card" style="width: 18rem;">
                 <img src="imagens/mvni.PNG" class="card-img-top" alt="Marcos Vinicius B.S.">
                 <h2>Marcos Vinicius Bartoli Senko</h2>
-                <p>< Some quick example text to build on the card title and make up
-                     the bulk of the card's content. /></p>
+                <p>
+                    < Some quick example text to build on the card title and make up
+                        the bulk of the card's content. />
+                </p>
             </div>
         </section>
 
         <section class="linguagem">
             <div class="card-linguagem card">
                 <h2><strong>linguagens de programação</strong></h2>
-                <p class="card-text">< Some quick example text to build on the card title and make up the bulk of the card's content. /></p>
+                <p class="card-text">
+                    < Some quick example text to build on the card title and make up the bulk of the card's content. />
+                </p>
 
 
                 <div class="icons-linguagem">
-                <p class="icon-html"><i class="fa-brands fa-html5" title="HTML5" alt="HTML5"></i></i></p>
-                <p class="icon-css"><i class="fa-brands fa-css3-alt" title="CSS" alt="CSS"></i></p>
-                <p class="icon-js"><i class="fa-brands fa-js" title="JavaScript" alt="JavaScript" ></i></p>
+                    <p class="icon-html"><i id="html" class="fa-brands fa-html5" title="HTML5" alt="HTML5"></i></i></p>
+                    <p class="icon-css"><i id="css" class="fa-brands fa-css3-alt" title="CSS" alt="CSS"></i></p>
+                    <p class="icon-js"><i id="js" class="fa-brands fa-js" title="JavaScript" alt="JavaScript"></i></p>
                 </div>
-            
+
             </div>
         </section>
 
