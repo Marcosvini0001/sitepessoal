697e84cf54d1bd540c59d5e4e0a1ee544920beef
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-09-06 22:39:55 -0300

    commit de segurança

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Desenvolvedor Marcos Vinicius Bartoli Senko / Programador Marcos Vinicius Bartoli Senko / Desenvolvedor em Campo Mourão PR / Programador em Campo Mourão PR">
 
     <base href="home">
 
