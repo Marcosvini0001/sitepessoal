918b41d181ecaa9c79bfec518a4a529c2ccbff63
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-09-11 18:20:32 -0300

    att

--- paginas/projetos.php
+++ paginas/projetos.php
@@ -4,7 +4,7 @@
         
         <div class="div-projetos">
             <h4>Projeto 1</h4>
-            <p>Status: </p>
+            <p>Status: <span>Em Desenvolvimento</span></p>
             <div class="card-projetos">
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, rerum commodi asperiores ex adipisci assumenda illo blanditiis aut.
                     Laboriosam odio accusamus eos eum necessitatibus repellendus magni mollitia id laudantium saepe?</p>
@@ -13,7 +13,7 @@
 
         <div class="div-projetos">
             <h4>Projeto 1</h4>
-            <p>Status: <span>Em Desenvolvimento</span> </p>
+            <p>Status: <span>Em Desenvolvimento</span></p>
             <div class="card-projetos">
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, rerum commodi asperiores ex adipisci assumenda illo blanditiis aut.
                     Laboriosam odio accusamus eos eum necessitatibus repellendus magni mollitia id laudantium saepe?</p>
