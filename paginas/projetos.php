57c971bc647d6f9e15827555e1bc1357de7f7e6c
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-09-12 17:53:06 -0300

    att seg

--- paginas/projetos.php
+++ paginas/projetos.php
@@ -1,4 +1,5 @@
 <main>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <section class="class-projetos">
 
@@ -9,9 +10,33 @@
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, rerum commodi asperiores ex adipisci assumenda illo blanditiis aut.
                     Laboriosam odio accusamus eos eum necessitatibus repellendus magni mollitia id laudantium saepe?</p>
             </div>
+
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="imagens/greenlife.png"  class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="imagens/greenlife.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="imagens/greenlife.png" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span id="span-slider" class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span id="span-slider" class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span id="span-slider" class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span id="span-slider" class="visually-hidden">Next</span>
+                </button>
+            </div>
+
             <div class="div-ling">
                 <p class="icon-html"><i id="html" class="fa-brands fa-html5" title="HTML5" alt="HTML5"></i></i></p>
                 <p class="icon-css"><i id="css" class="fa-brands fa-css3-alt" title="CSS" alt="CSS"></i></p>
+                <p class="icon-js"><i id="js" class="fa-brands fa-js" title="JavaScript" alt="JavaScript"></i></p>
             </div>
         </div>
 
@@ -22,9 +47,33 @@
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, rerum commodi asperiores ex adipisci assumenda illo blanditiis aut.
                     Laboriosam odio accusamus eos eum necessitatibus repellendus magni mollitia id laudantium saepe?</p>
             </div>
+
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="imagens/logo.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="imagens/logo.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="imagens/logo.png" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span id="span-slider" class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span id="span-slider" class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span id="span-slider" class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span id="span-slider" class="visually-hidden">Next</span>
+                </button>
+            </div>
+
             <div class="div-ling">
                 <p class="icon-html"><i id="html" class="fa-brands fa-html5" title="HTML5" alt="HTML5"></i></i></p>
                 <p class="icon-css"><i id="css" class="fa-brands fa-css3-alt" title="CSS" alt="CSS"></i></p>
+                <p class="icon-js"><i id="js" class="fa-brands fa-js" title="JavaScript" alt="JavaScript"></i></p>
             </div>
 
         </div>
